2d10f73a68e76d6f9dad84c0df3cf3184f759c88
AbhinavVohra <abhinavvohra08@gmail.com>
2026-08-11 23:39:23 +0530

    Fix formatting in DAY 2 q1.c

--- DAY 2 q1.c
+++ DAY 2 q1.c
@@ -1,6 +1,6 @@
 //Write a program to calculate the area and perimeter of a rectangle given its length and breadth.
 
-#include <stdio.h>
+  #include <stdio.h>
 
 int main() {
   int length,breadth,area,perimeter;
