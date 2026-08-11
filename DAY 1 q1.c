b4e5eed641571e6a9b9a72b5dc5d94eaf8db059d
AbhinavVohra <abhinavvohra08@gmail.com>
2026-08-11 22:30:06 +0530

    Implement sum of two numbers in DAY 1 q1.c
    The program takes two integers as input and displays their sum.

---
+++ DAY 1 q1.c
@@ -0,0 +1,11 @@
+//Write a program to input two numbers and display their sum.
+
+#include <stdio.h>
+
+int main() {
+  int a,b,sum;
+  scanf("%d,%d",&a,&b);
+  sum= a + b;
+  printf("sum = %d\n",sum);
+  return 0;
+}
