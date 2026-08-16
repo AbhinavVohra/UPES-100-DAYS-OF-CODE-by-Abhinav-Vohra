f720b3b11715870e71d11e3a9c7ec2c4954489e5
Abhinav Vohra <abhinavvohra08@gmail.com>
2026-08-16 13:32:56 +0530

    Modify calculations to support float inputs
    Changed variable types to float for decimal support and updated print statements for formatting.

--- DAY 1 q2.c
+++ DAY 1 q2.c
@@ -3,13 +3,17 @@
 #include <stdio.h>
 
 int main() {
-  int a,b;
-  scanf("%d %d",&a,&b);
+  float a,b;                  //Number can be both in integer or decimal form,that,s why it is safe to take the data type as float
+  printf("Enter the value of the first number:");
+  scanf("%f",&a);
 
-  printf("sum = %d\n",a+b);
-  printf("difference = %d\n",a-b);
-  printf("product = %d\n",a*b);
-  printf("quotient = %d\n",a/b);
+  printf("Enter the value of the second number:");
+  scanf("%f",&b);
+
+  printf("sum = %.2f\n",a+b);                     //Sum will be integer or decimal depending upon the numbers
+  printf("difference = %.2f\n",a-b);                //The difference will be integer or decimal depending upon the numbers
+  printf("product = %.2f\n",a*b);                     //The product will be integer or decimal depending upon the numbers
+  printf("quotient = %.2f\n",a/b);                  //The remainder will be integer or decimal depending upon the numbers
 
   return 0;
 }
