56ad67cfdaeade9840c432afd734924c73369d5b
Abhinav Vohra <abhinavvohra08@gmail.com>
2026-09-25 22:08:36 +0530

    Create DAY 47 q2.c

---
+++ DAY 47 q2.c
@@ -0,0 +1,17 @@
+//Write a program to find the largest word in a sentence
+
+#include <stdio.h>
+#include <string.h>
+int main() {
+  char str[100];
+
+  printf("Enter a string/sentence : ");
+  scanf("%s",&str);
+
+  int n = sizeof(str);
+  int max = strlen();
+
+  for (int i = '\0' ; i < n ; i = i + 1) {
+       if
+
+  
